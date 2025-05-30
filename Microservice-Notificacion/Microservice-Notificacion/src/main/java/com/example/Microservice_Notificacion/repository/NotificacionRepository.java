dcdf43a7fea5e09b0a79affaa491e2607b10dd14
Ignacio Contreras <ignaciocontreras.iacne@gmail.com>
2025-05-30 16:50:20 -0400

    se elimina microservicio Notificacion

--- Microservice-Notificacion/Microservice-Notificacion/src/main/java/com/example/Microservice_Notificacion/repository/NotificacionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Microservice_Notificacion.repository;
-
-import com.example.Microservice_Notificacion.model.Notificacion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NotificacionRepository extends JpaRepository<Notificacion, Long> {
-}
