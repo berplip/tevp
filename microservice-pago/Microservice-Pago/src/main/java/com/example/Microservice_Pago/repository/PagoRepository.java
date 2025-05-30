a6d392c719a5c5a8066c50a51d1dd144d19277fe
Ignacio Contreras <ignaciocontreras.iacne@gmail.com>
2025-05-30 16:51:47 -0400

    se elimina microservicio Pago

--- microservice-pago/Microservice-Pago/src/main/java/com/example/Microservice_Pago/repository/PagoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Microservice_Pago.repository;
-
-import com.example.Microservice_Pago.model.Pago;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PagoRepository extends JpaRepository<Pago, Long> {
-}
