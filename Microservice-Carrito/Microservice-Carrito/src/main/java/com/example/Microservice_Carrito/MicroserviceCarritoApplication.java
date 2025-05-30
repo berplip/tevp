9a12d78ae75f41ae0719b63e5025ad8ceb599acf
Ignacio Contreras <ignaciocontreras.iacne@gmail.com>
2025-05-30 16:42:51 -0400

    se añade nuevo microservicio Carrito

---
+++ Microservice-Carrito/Microservice-Carrito/src/main/java/com/example/Microservice_Carrito/MicroserviceCarritoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Microservice_Carrito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceCarritoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceCarritoApplication.class, args);
+	}
+
+}
