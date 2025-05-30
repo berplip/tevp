d696805d7470233721be9944491d4ce576007ab7
Ignacio Contreras <ignaciocontreras.iacne@gmail.com>
2025-05-30 16:39:54 -0400

    se elimina microservicio carrito

--- microservice-carrito/Microservice-Carrito/src/main/java/com/example/Microservice_Carrito/repository/CarritoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Microservice_Carrito.repository;
-
-import com.example.Microservice_Carrito.model.Carrito;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CarritoRepository extends JpaRepository<Carrito, Long> {
-}
