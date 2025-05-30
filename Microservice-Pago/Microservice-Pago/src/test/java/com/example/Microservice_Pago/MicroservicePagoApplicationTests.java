52f032c2bdcf92c2d2aa0ac4a6d8cde49070064d
Ignacio Contreras <ignaciocontreras.iacne@gmail.com>
2025-05-30 16:52:27 -0400

    se añade nuevo microservicio Pago

---
+++ Microservice-Pago/Microservice-Pago/src/test/java/com/example/Microservice_Pago/MicroservicePagoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Microservice_Pago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicePagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
