d532d1723f12c32ffb4b9af509b2a2feb2f82266
Ignacio Contreras <ignaciocontreras.iacne@gmail.com>
2025-05-30 16:50:55 -0400

    se añade nuevo microservicio Notificacion

---
+++ Microservice-Notificacion/Microservice-Notificacion/src/test/java/com/example/Microservice_Notificacion/MicroserviceNotificacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Microservice_Notificacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceNotificacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
